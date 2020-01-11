2c9e7a792989b103ea1d561b58c2fe38ff78a54c
LeDima <484404@gmail.com>
2020-01-11 11:25:16 +0200

    Commit

---
+++ MousePosition.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 11:21:48 2020
+
+@author: LeDima
+"""
+
+import sys
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget)
+
+
+class MouseTracker(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setMouseTracking(True)
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Mouse Tracker')
+        self.label = QLabel(self)
+        self.label.resize(200, 40)
+        self.show()
+
+    def mouseMoveEvent(self, event):
+        self.label.setText('Mouse coords: ( %d : %d )' % (event.x(), event.y()))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MouseTracker()
+    sys.exit(app.exec_())
